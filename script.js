d36779afc002522275fd5f158cd37e7379466146
U-Michal-Komputer\Michal <Michal@Michal-Komputer.(none)>
2010-08-28 03:40:53 +0200

    table draw

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-(function(){
- document.getElementById('test').innerHTML = 'IT WORKS!';
- })();
+ //IE fix Array.indeOf
+ if(!Array.indexOf){
+    Array.prototype.indexOf = function(obj){
+        for(var i=0; i<this.length; i++){
+            if(this[i]==obj){
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
+
+// Main Variables
+var xFields = 10,
+    yFields = 20,
+    fieldSize = 15,
+    container = document.getElementById('container'),
+    //construction Vars
+    table = document.createElement('table'),
+    tbody = document.createElement('tbody');
+
+container.appendChild(table); 
+table.appendChild(tbody);
+
+for( var y = 0; y < yFields; y++ ) { //'draw' rows
+    var row = document.createElement('tr');
+    tbody.appendChild(row);
+    for( var x = 0; x < xFields; x++ ) { //and cols
+        var field = document.createElement('td');
+        field.style.width = field.style.height = fieldSize;
+        field.id = x+"_"+y;
+        row.appendChild(field);
+    }
+}
